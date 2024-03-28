ae699a3c1bfe45b1bfb4ff1999c570081b1e6071
EduCuba <evidalcc@gmail.com>
2024-03-28 00:16:43 -0500

    commit

--- registroevento/settings.py
+++ registroevento/settings.py
@@ -70,8 +70,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
      
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -216,20 +216,31 @@ USE_TZ = True
  #   },
 #}
 
-
+# Sacado de staticfile_dirs: '/var/www/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/'
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        #"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+#STATIC_URL = "static/"
+#STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
+##if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    ##STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    ##STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_ROOT = {BASE_DIR /'media'}
