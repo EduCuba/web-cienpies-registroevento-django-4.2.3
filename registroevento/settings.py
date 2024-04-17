e3b23ded9a8aa18aae3bbe7c0a0d060a5e3e501e
EduCuba <evidalcc@gmail.com>
2024-04-17 11:58:24 -0500

    commit

--- registroevento/settings.py
+++ registroevento/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 ####import pymysql
 #Agregado para conectar a base remoto desde app local
-import dj_database_url
+#ok render import dj_database_url
 #agregado para render
 import os
 
